802a1d1e02d594b3f3d3a1af550e5816dce02df1
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-26 12:19:37 +0600

    13 looping data (v-for)

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const { createApp} = Vue;
 createApp({
     data(){
         return {
-            age: 29,
+            
             name: "Sohan",
+            students: ["Sohan", "Sujan", "Bishu", "Pasha","Manna"],
 
          };
     },
-methods: {},
 
 }).mount("#app-root");
 
