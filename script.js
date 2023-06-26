8817b19b07710e56b2c61c99e395e131554c0221
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-26 11:43:08 +0600

    12 if conditions & v-show part01

--- script.js
+++ script.js
@@ -3,23 +3,12 @@ const { createApp} = Vue;
 createApp({
     data(){
         return {
-            /*
-            title: "my_title",
-            myColor:"blue",*/
-            myFont: 15,
-        };
+            age: 29,
+            name: "Sohan",
+
+         };
     },
 methods: {},
-computed: {
-    numOneChange(){
-        console.log("First function fired");
-        return this.numberOne * 2;
-    },
-    numTwoChange(){
-        console.log("Second function fired");
-        return this.numberTwo + 10;
-    },
-},
 
 }).mount("#app-root");
 
