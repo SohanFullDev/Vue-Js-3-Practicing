fec04cf5e3c737302c489d0f01b782733f5604a8
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-26 14:23:02 +0600

    14 looping data (v-for)

--- script.js
+++ script.js
@@ -3,26 +3,46 @@ const { createApp} = Vue;
 createApp({
     data(){
         return {
-            
-            name: "Sohan",
-            students: [
-                {
-                    name: "Sohan",
-                    id: 1,
-                    age: 12,
-                },
-                {
-                    name: "Sujan",
-                    id: 2,
-                    age: 25,
-                },
-                {
-                    name: "Salman",
-                    id: 3,
-                    age: 27,
-                }
+          products: [
+            {
+                title: "Symphony Phone",
+                description: "This is a very cool shoes with a cool color",
+                price: "50$",
+                brands: "Adidass",
+                img: './img/pic01.jpg',
+                id: 1,
 
-            ],
+            },
+            {
+                title: "Samsung Mobile",
+                description: "This is a Samsung Mobile with a cool color",
+                price: "500$",
+                brands: "Samsung",
+                img: './img/pic02.png',
+                id: 2,
+
+            },
+            {
+                title: "Apple Mobile",
+                description: "This is a Apple Mobile with a cool color",
+                price: "150$",
+                brands: "Apple",
+                img: './img/pic03.jpg',
+                id: 3,
+
+            },
+            {
+                title: "Xiami Mobile",
+                description: "This is a Xiami Mobile with a cool color",
+                price: "150$",
+                brands: "Xiami",
+                img: './img/pic04.jpg',
+                id: 3,
+
+            },
+
+          ],
+                
 
          };
     },
