b8416b442e577d51dc46740c5b0277b8508096ff
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-02 11:02:15 +0600

    21 components-big-intro_t18_part03

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ const my_component = {
     template: `<h2> Hello from my first component</h2>
         <h3>My Name is {{ name }} </h3>
         <h3>My Age is {{ age }} </h3>
+        <button @click='changeAge'>click me</button>
     `,
     data(){
         return {
             name: "Sohan",
             age: 28,
         };
+    },
+    methods: {
+        changeAge(){
+            this.age++;
+        },
+
     },
 
 };
@@ -23,3 +30,13 @@ const vue_1 = createApp({
 
     },
 }).mount("#app_root");
+
+const vue_2 = createApp({
+    data(){
+        return {};
+    },
+    components: {
+        my_component,
+
+    },
+}).mount("#app_root_2");
