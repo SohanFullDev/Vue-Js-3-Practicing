253311c29babff80cc40cd9d3a6b2f0c5202d210
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-02 14:31:39 +0600

    24 vue-cli(views & routes)_t21

---
+++ vue-cli/src/views/MyMain.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="my-main">
+    <h1>This is my main page.</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+      tenetur? Enim, quae fuga facere maxime dignissimos dolore nihil incidunt
+      dolor non unde iste odit, labore tenetur ipsam earum amet cum?
+    </p>
+  </div>
+</template>
