8a3c99b8dc044df1e128966ea9094717fa6054ed
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-02 10:53:38 +0600

    20 components-big-intro_t18_part02

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const { createApp} = Vue;
 
 const my_component = {
-    template: "<h2> Hello from my first component</h2>"
+    template: `<h2> Hello from my first component</h2>
+        <h3>My Name is {{ name }} </h3>
+        <h3>My Age is {{ age }} </h3>
+    `,
+    data(){
+        return {
+            name: "Sohan",
+            age: 28,
+        };
+    },
 
 };
 
