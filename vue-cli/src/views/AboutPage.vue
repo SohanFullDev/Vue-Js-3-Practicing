366693b9eb981256af6045dcfdc816a38816f75c
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-03 14:59:07 +0600

    31 vue-cli(props)_props03_t25

--- vue-cli/src/views/AboutPage.vue
+++ vue-cli/src/views/AboutPage.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h2>This is about page</h2>
+    <MyCont :st="students" />
   </div>
 </template>
+<script>
+import MyCont from "@/components/MyContent.vue";
+export default {
+  components: {
+    MyCont,
+  },
+  data() {
+    return {
+      students: ["Rubel", "Kamal", "Jamal", "Roman", "Suman"],
+    };
+  },
+};
+</script>
